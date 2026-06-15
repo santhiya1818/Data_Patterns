8295802b48f14202feee6c82682fb8bbe442dd88
Santhiya Sri.C.S <jayanthiseenivasan1325_bcs27@mepcoeng.ac.in>
2026-06-15 23:32:31 +0530

    Update Frequent alphabet.py

--- Data Patterns/Frequent alphabet.py
+++ Data Patterns/Frequent alphabet.py
@@ -1,25 +1,14 @@
 line = input("Enter a line: ")
-
 freq = {}
-
 for ch in line:
-
     if ch.isalpha():
-
         freq[ch] = freq.get(ch, 0) + 1
-
 max_freq = max(freq.values())
-
 result = []
-
 for ch in freq:
-
     if freq[ch] == max_freq:
         result.append(ch)
-
 result.sort(key=lambda x: (x.islower(), x))
-
 print("Highest Frequency Letters:")
-
 for ch in result:
     print(ch, end=" ")
